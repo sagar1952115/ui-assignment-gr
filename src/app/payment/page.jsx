42deda63224b3078d7484358827adac343bb9293
sagar1952115 <sagarkumar1952115@gmail.com>
2024-03-16 00:23:17 +0530

    resolved redirection issue

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -37,11 +37,11 @@ const Payment = () => {
     getRandomStatus();
     setOpenModal(true);
   };
-  useEffect(() => {
-    if (products.length === 0) {
-      router.push("/");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (products.length === 0) {
+  //     router.push("/");
+  //   }
+  // }, []);
   return (
     <>
       <div className="flex min-h-screen   justify-between w-full h-full flex-col md:rounded-md  bg-white">
